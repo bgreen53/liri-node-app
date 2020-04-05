b9aa1bda39322a4b7d881ed774b8d586dd3ba35f
bgreen53 <green.barry53@gmail.com>
2020-04-04 23:25:17 -0700

    spotify function operational

--- liri.js
+++ liri.js
@@ -1,13 +1,14 @@
-// require("dotenv").config();
+require("dotenv").config();
 var keys = require("./keys.js");
 var fs= require("fs")
 const axios = require('axios');
+var Spotify = require('node-spotify-api');
 
 var cmd = process.argv[2]
-var artist= process.argv[3]
+var term= process.argv[3]
 
  var concert = function() {
-        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+        axios.get("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp").then(
   function(response) {
     var data = response.data
     for(var i=0; i<data.length;i++){
@@ -18,10 +19,37 @@ var artist= process.argv[3]
 }
   })
 };
-   
 
+
+var spotify = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret,
+  });
+
+  
+var song = function(){
+spotify
+.search({ type: 'track', query: term })
+.then(function(response) {
+var songData = response.tracks.items
+for(var i=0; i<songData.length;i++){
+console.log("Artist: " +songData[i].artists[0].name); 
+console.log("Song: " +songData[i].name);   
+console.log("Album: " +songData[i].album.name);
+console.log("Preview: " +songData[i].preview_url);
+console.log("\n-----------\n")
+}
+})
+.catch(function(err) {
+  console.log(err);
+});
+}
 
 if(cmd==="concert-this"){
     concert()
+
+}
+if(cmd==="spotify-this-song"){
+    song()
 
 }
