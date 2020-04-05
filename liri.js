f76e9c469cd3102a0d704b84c741d56a88c6abeb
bgreen53 <green.barry53@gmail.com>
2020-04-04 22:51:17 -0700

    concert function operational

--- liri.js
+++ liri.js
@@ -1,5 +1,27 @@
-require("dotenv").config();
+// require("dotenv").config();
 var keys = require("./keys.js");
 var fs= require("fs")
+const axios = require('axios');
+
+var cmd = process.argv[2]
+var artist= process.argv[3]
+
+ var concert = function() {
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
+  function(response) {
+    var data = response.data
+    for(var i=0; i<data.length;i++){
+    console.log("Venue Name: "+ data[i].venue.name);
+    console.log("Venue Location: "+ data[i].venue.city)
+    console.log("Concert Date: " + data[i].datetime)
+    console.log("\n-----------\n")
+}
+  })
+};
+   
 
 
+if(cmd==="concert-this"){
+    concert()
+
+}
